3c9239d556ac45ec9b330b43bb4e403dacd11b11
jyx- <2283715017@qq.com>
2024-02-20 14:20:15 +0800

    1.0

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <router-view />
+
+</template>
+
+<script lang="ts" setup>
+import {
+ Document,
+ Menu as IconMenu,
+ Location,
+} from '@element-plus/icons-vue'
+
+
+const date = new Date()
+   const dateStr = `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`
+    
+</script>
+
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+ width: 200px;
+ min-height: 400px;
+}
+</style>
